eca31a30f0606d9a2e72c9e29a0b09476622120e
David <david@Davids-MacBook-Pro.local>
2023-10-10 15:20:34 -0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header"></header>
+      <body className="App-body">
+        <iframe
+          title="ScrollNT-Embed"
+          className="ScrollNT-embed"
+          width="800"
+          height="450"
+          src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2F86BZeSTF6fubDud5QDhPEt%2FScrollNT-II%3Fnode-id%3D37-2%26starting-point-node-id%3D13%253A183%26scaling%3Dscale-down%26mode%3Ddesign%26t%3DJzmZcQv8IzmIbpu1-1"
+        ></iframe>
+        <iframe
+          title="Qualtrics-Embed"
+          className="Qualtrics-embed"
+          src="https://oregonstate.qualtrics.com/jfe/form/SV_0BU2a1XQslhHGeO"
+        ></iframe>
+      </body>
     </div>
   );
 }
