d273a189f4547cd783fb285b98f0d2cb63acecd0
David <david@Davids-MacBook-Pro.local>
2023-10-10 20:44:04 -0700

    Add buttons

--- src/EndTestModal.jsx
+++ src/EndTestModal.jsx
@@ -5,8 +5,14 @@ const EndTestModal = ({ onClose }) =>
   createPortal(
     <div className="modal">
       <div className="modal-content">
-        <div>I'm a modal dialog</div>
-        <button onClick={() => onClose()}>Close</button>
+        <div>
+          Are you sure you are done? Click cancel unless you mean to be done and
+          you have clicked "submit" on the Qualtrics survey.
+        </div>
+        <div className="buttons">
+          <button onClick={() => onClose()}>Close</button>
+          <button onClick={() => onClose()}>End Activity</button>
+        </div>
       </div>
     </div>,
     document.getElementById("modal-root")
