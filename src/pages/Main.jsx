7cf3e9b804ab158cf454625d9bc780aeef7d76c0
David <david@Davids-MacBook-Pro.local>
2023-10-12 13:36:29 -0700

    BOOTLEG instagram simulator

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,5 +1,6 @@
 import "./Main.css";
-import { useEffect, useLayoutEffect, useState, useRef } from "react";
+import { useEffect, useState, useRef } from "react";
+import SocialFeedSimulator from "../scrollnt-web/SocialFeedSimulator";
 
 const Main = ({ isExperimentGroup }) => {
   // https://timetoprogram.com/get-width-of-element-react-js/
@@ -9,6 +10,7 @@ const Main = ({ isExperimentGroup }) => {
   const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
   const [position, setPosition] = useState({ x: 0, y: 0 });
 
+  // Component dimensions
   const getComponentDimensions = () => {
     setDimensions({
       width: elementRef.current?.offsetWidth,
@@ -28,6 +30,7 @@ const Main = ({ isExperimentGroup }) => {
     };
   }, []);
 
+  // Interval timer
   useEffect(() => {
     const interval = setInterval(() => {}, 1000);
 
@@ -38,23 +41,15 @@ const Main = ({ isExperimentGroup }) => {
 
   return (
     <body>
-      <div
-        id="Main-body"
-        className="Main-body"
-        ref={elementRef}
-        onMouseMove={(ev) =>
-          setMousePosition({
-            x: (ev.clientX - position.x) / dimensions.width,
-            y: (ev.clientY - position.y) / dimensions.height,
-          })
-        }
-      >
+      <div id="Main-body" className="Main-body" ref={elementRef}>
         <div
           onMouseEnter={() => console.log("Interacting with Figma")}
           onMouseLeave={() => console.log("Leaving Figma")}
+        
         >
           {isExperimentGroup ? (
             <iframe
+              id="ScrollNT-Embed"
               title="ScrollNT-Embed"
               className="ScrollNT-embed"
               width="800"
@@ -62,13 +57,7 @@ const Main = ({ isExperimentGroup }) => {
               src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2F86BZeSTF6fubDud5QDhPEt%2FScrollNT-II%3Fnode-id%3D37-2%26starting-point-node-id%3D13%253A183%26scaling%3Dscale-down%26mode%3Ddesign%26t%3DJzmZcQv8IzmIbpu1-1"
             ></iframe>
           ) : (
-            <iframe
-              title="ScrollNT-Embed"
-              className="ScrollNT-embed"
-              width="800"
-              height="450"
-              src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FfsI207s18RN37WLGZx12Oo%2FScrollNT-II-(Control)%3Ftype%3Ddesign%26t%3DMuY5HYzPfb2Cnld0-1%26scaling%3Dscale-down%26page-id%3D0%253A1%26starting-point-node-id%3D13%253A183%26node-id%3D13-183%26mode%3Ddesign"
-            ></iframe>
+            <SocialFeedSimulator className="ScrollNT-embed" />
           )}
         </div>
 
@@ -77,6 +66,7 @@ const Main = ({ isExperimentGroup }) => {
           onMouseLeave={() => console.log("Leaving Qualtrics")}
         >
           <iframe
+            id="Qualtrics-Embed"
             title="Qualtrics-Embed"
             className="Qualtrics-embed"
             src="https://oregonstate.qualtrics.com/jfe/form/SV_0BU2a1XQslhHGeO"
---
+++ src/scrollnt-web/SocialFeedSimulator.js
@@ -0,0 +1,70 @@
+import React, { useState, useRef } from "react";
+import { VariableSizeList } from "react-window";
+
+import {
+  StatusBar,
+  useColorScheme,
+  View,
+  Image,
+  Dimensions,
+  AppState,
+} from "react-native-web";
+import postRefs from "./postRefs";
+import { Masonry } from "@mui/lab";
+
+const SocialFeedSimulator = () => {
+  const snsSimulator = useRef();
+  const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
+
+  const [posts, setPosts] = useState(postRefs);
+
+  const headerHeight = ((window.innerWidth * 0.35) / 1125) * 182;
+  const footerHeight = ((window.innerWidth * 0.35) / 1125) * 159;
+
+  const Row = ({ index, style }) => {
+    const p = posts[index].width / posts[index].height;
+
+    return (
+      <Image style={style} source={posts[index].image} resizeMode="cover" />
+    );
+  };
+
+  return (
+    <div
+    // style={{
+    //   flexDirection: "row",
+    //   alignItems: "center",
+    //   width: "100%",
+    //   height: "100%",
+    // }}
+    >
+      <Image
+        source={require("./res/images/header.png")}
+        resizeMode="cover"
+        style={{ width: window.innerWidth * 0.35, height: headerHeight - 30 }}
+      />
+
+      <VariableSizeList
+        overscanCount={2}
+        height={window.innerHeight * 0.7}
+        width={window.innerWidth * 0.35}
+        itemCount={10}
+        itemSize={(index) =>
+          (posts[index].height * 0.35 * window.innerWidth) / posts[index].width
+        }
+      >
+        {Row}
+      </VariableSizeList>
+
+      <Image
+        source={require("./res/images/footer.png")}
+        style={{
+          width: window.innerWidth * 0.35,
+          height: footerHeight,
+        }}
+      />
+    </div>
+  );
+};
+
+export default SocialFeedSimulator;
