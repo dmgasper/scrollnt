c678da460c25cbc9693ed34d37c38ec1e3825134
David <david@Davids-MacBook-Pro.local>
2023-10-13 07:16:52 -0700

    Write basic test

--- src/App.test.js
+++ src/App.test.js
@@ -1,2 +1,8 @@
+import { render, screen } from "@testing-library/react";
+import { App } from "./App";
 
-
+test("renders title", () => {
+  render(<App />);
+  const titleElement = screen.getByText(/Attention Activity/i);
+  expect(titleElement).toBeInTheDocument();
+});
