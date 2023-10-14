85e3ee4386b4e05b37f9ce816b643694a799be11
David <david@Davids-MacBook-Pro.local>
2023-10-14 10:35:43 -0700

    Update build yaml

--- scrollnt/app.js
+++ scrollnt/app.js
@@ -17,15 +17,10 @@ async function getApp() {
   const path = __dirname + "/views/";
   const app = express();
 
-  // view engine setup
-  // app.set("views", path.join(__dirname, "views"));
-  // app.set("view engine", "jade");
-
   app.use(logger("dev"));
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
   app.use(cookieParser());
-  // app.use(express.static(path.join(__dirname, "public")));
   app.use(express.static(path));
 
   app.use("/", indexRouter);
